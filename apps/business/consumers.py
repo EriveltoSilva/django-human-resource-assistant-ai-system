37a7d389716eb08279597a9d49259ad3042e54ec
eriveltosilva <eriveltoclenio@gmail.com>
2024-05-20 03:08:05 +0100

    feat: cvs analyses

--- apps/business/consumers.py
+++ apps/business/consumers.py
@@ -1,16 +1,11 @@
 """  consumers for chat analysis """
 import json
 
-from asgiref.sync import async_to_sync, sync_to_async
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.shortcuts import get_list_or_404
 
 from .utils import CVAnalyzer
 from .models import Vacancy, Candidate
-
-# from .utils import ChatPDFLangchain
-# from .models import Chat, ChatMessage
-# from .models import Message, Room, ArquivoPDF
 
 def deb(obj):
     print("#"*100)
@@ -18,7 +13,9 @@ def deb(obj):
     print("#"*100)
 
 class CandidateAnalysisConsumer(AsyncWebsocketConsumer):
+    """Consumer for chat analysis"""
     async def connect(self):
+        """ AsyncWebsocketConsumer method to connect """
         self.vacancy_id = self.scope['url_route']['kwargs']['vid']
         self.num_docs = self.scope['url_route']['kwargs']['num_documents']
         self.username = "Anonymous"
@@ -45,8 +42,6 @@ class CandidateAnalysisConsumer(AsyncWebsocketConsumer):
         await self.accept()
         
 
-
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
     
@@ -54,56 +49,50 @@ class CandidateAnalysisConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None):
         data = json.loads(text_data)
         message = data['message']
-
         #Send message to room group
         await self.channel_layer.group_send(self.room_group_name,{'type':'chat_message','message': message})
 
 
-    #Receive message from web socket
+    #Send message to chat group
     async def chat_message(self, event):
         if event['message'] == 'data':
             deb(f"received message {event['message']}")
+            
+            # Get the docs pdf
             relevant_docs = await self.chatbot.similar_docs(self.job_description, self.num_docs, self.vectorstore, self.scope['cookies']['sessionid'])
             response = await self.get_response_formatted(relevant_docs)
+            deb(f"SENDING RESPONSE TO FRONT...")
             await self.send(text_data=json.dumps({
                 'type': 'response',
                 'message': "Estes são os candidatos que melhor se adequam a vaga!",
                 'data':response
-            }))
-            deb("RESPOSTAS 5")
-
-        # #Send message to Websockets
-        # await self.send(text_data=json.dumps({
-        #     'type':'response',
-        #     'message': event['chatbot_response'],
-        #     'username': self.scope.get('user').__str__()
-        # }))
-    
-    
+            }))    
 
     # ************************************** My Custom methods  **********************************
     @sync_to_async
     def get_vacancy_instance(self):
+        """ get vacancy instance that candidates applied """
         return Vacancy.objects.get(vid=self.vacancy_id)
 
     @sync_to_async
     def get_job_description(self) -> str:
+        """ get all string text description """
         return self.vacancy.get_job_description()
     
     @sync_to_async
     def get_candidacies(self):
+        """ get all candidacies"""
         candidates = Candidate.objects.filter(vacancy=self.vacancy)
         candidacy_list = [candidate for candidate in candidates]
         return candidacy_list
     
-    # @sync_to_async
     async def get_response_formatted(self, relevant_docs):
         items = []
         for item in range(len(relevant_docs)):
             items.append({
                 'header': f"👉 {str(item+1)}",
                 'filename': str(relevant_docs[item][0].metadata['name']),
-                'compatibility': round(float(relevant_docs[item][1])*100, 2),
+                'compatibility': f"{round(float(relevant_docs[item][1])*100, 2)}%",
                 'summary': await self.chatbot.get_summary([relevant_docs[item][0]])
             })
         return items
--- local_static/assets/business/js/candidacy_analyse.js
+++ local_static/assets/business/js/candidacy_analyse.js
@@ -1,209 +1,82 @@
 /***************************************************** Variables  ****************************************************/
-// const chatInput = document.querySelector("#chat-input");
-// const sendButton = document.querySelector("#send-btn");
-// const chatContainer = document.querySelector(".chat-container");
-// const themeButton = document.querySelector("#theme-btn");
-// const deleteButton = document.querySelector("#delete-btn");
-// const initialInputHeight = chatInput.scrollHeight;
-
-
 const vacancy_vid = JSON.parse(document.getElementById('json-vacancy_vid').textContent)
 const numDocuments = document.getElementById('numDocuments');
 const btnStart = document.getElementById('btnStart');
 let chatSocket =null;
 const container = document.getElementById('default-chat-container');
+const rows = document.querySelectorAll("table tbody tr");
 
-
-const createAlert = (text)=>{
-    let p = document.createElement('p');
-    p.innertHTML = ""
-    p.className = 'alert alert-success';
-    p.textContent = text;
-    return p;
+const removeForm = ()=>{
+    document.getElementById('formContainer').remove();
 }
-btnStart.addEventListener('click', () => {
-    if(!numDocuments.value)
-        {
-            alert("Preencha o número de documentos que devem ser retornados!");
-            return;
-        }
-        
-        chatSocket = new WebSocket(`ws://${window.location.host}/ws/analisar-candidaturas/${vacancy_vid}/${numDocuments.value}/`);
-        
-        chatSocket.onopen = function(e) {
-            console.log('Conexão estabelecida.');
-            // window.alert('Conexão estabelecida.');
 
-            let child = `
-                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                Processando...
-            `;
-            btnStart.innerHTML=``;
-            btnStart.setAttribute('disabled', true);
-            btnStart.insertAdjacentHTML('beforeend', child);
-            chatSocket.send(JSON.stringify({"message": "data"}))
-        };
-        
-        
-        chatSocket.onmessage = function (e) {
-            const data = JSON.parse(e.data)
-            console.log("Data Received:", data);
+const alterTable = (data)=>{
+    rows.forEach(row => {
+        const link = row.querySelector("a");
+        if (link) {
+            const href = link.getAttribute('href');
+            const match = data.find(item => href.includes(item.filename));
+            if (match) {
+                const compatibilityCell = row.querySelector('.compatibility');
+                const summaryCell = row.querySelector('.summary');
 
-            if(data.type==='response')
-            {
-                container.innerHTML = "";
-                const child=`
-                    <p class="alert alert-success">
-                        Os Dados chegaram
-                    </p>
-                `;
-                container.insertAdjacentHTML('beforeend', child);
+                compatibilityCell.textContent = match.compatibility;
+                summaryCell.textContent = match.summary;
+
+                compatibilityCell.classList.add('alert-success');
+                summaryCell.classList.add('alert-success');
+            } else {
+                row.remove();
             }
         }
-        
-        chatSocket.onclose = function() {
-            console.log('Conexão fechada.');
-            // window.alert("Conexão fechada");
-        };
-        
     });
-
-// chatSocket.send(JSON.stringify({"message": userText, "room_name":chatID, "username":username}))
-
-// let userText = null;
-// let chatResponseElement = null;
-
-/***************************************************** FunctionS  ****************************************************/
-// const clearInput = () => {
-    //     chatInput.value = "";
-//     chatInput.style.height = `${initialInputHeight}px`;
-// }
-
-// const copyResponse = (copyBtn) => {
-//     const reponseTextElement = copyBtn.parentElement.querySelector("p");
-//     navigator.clipboard.writeText(reponseTextElement.textContent);
-//     copyBtn.textContent = "done";
-//     setTimeout(() => copyBtn.textContent = "content_copy", 1000);
-// }
-
-// const createChatElement = (content, className) => {
-//     const chatDiv = document.createElement("div");
-//     chatDiv.classList.add("chat", className);
-//     chatDiv.innerHTML = content;
-//     return chatDiv;
-// }
-
-// const loadDataFromLocalstorage = () => {
-//     document.body.classList.toggle("light-mode", localStorage.getItem("themeColor") === "light_mode");
-//     if (localStorage.getItem("all-chats"))
-//         chatContainer.innerHTML = localStorage.getItem("all-chats");
-//     chatContainer.scrollTo(0, chatContainer.scrollHeight); // Scroll to bottom of the chat container
-// }
-
-// const showTypingAnimation = () => {
-//     const html = `<div class="chat-content">
-//     <div class="chat-details">
-//     <img src="/static/assets/images/chatbot.png" alt="chatbot-img">
-//     <div class="typing-animation">
-//     <div class="typing-dot" style="--delay: 0.2s"></div>
-//     <div class="typing-dot" style="--delay: 0.3s"></div>
-//     <div class="typing-dot" style="--delay: 0.4s"></div>
-//     </div>
-//     </div>
-//     <span onclick="copyResponse(this)"><i class="bi bi-copy"></i></span>
-//     </div>`;
-//     const incomingChatDiv = createChatElement(html, "incoming");
-//     chatContainer.appendChild(incomingChatDiv);
-//     chatContainer.scrollTo(0, chatContainer.scrollHeight);
-//     chatResponseElement = incomingChatDiv;
-//     // getChatResponse(incomingChatDiv);
-// }
-
-// const handleOutgoingChat = () => {
-//     userText = chatInput.value.trim();
-//     if (!userText) return;
-
-//     clearInput();
-
-//     const html = `<div class="chat-content">
-//     <div class="chat-details">
-//     <img src="/static/assets/images/user.png" alt="user-img">
-//     <p>${userText}</p>
-//     </div>
-//     </div>`;
-
-//     const outgoingChatDiv = createChatElement(html, "outgoing");
-//     chatContainer.querySelector(".default-text")?.remove();
-//     chatContainer.appendChild(outgoingChatDiv);
-//     chatContainer.scrollTo(0, chatContainer.scrollHeight);
-
-//     chatSocket.send(JSON.stringify({"message": userText, "room_name":roomName, "username":username}))
-//     showTypingAnimation();
-// }
-
-// const chatSocket = new WebSocket(`ws://${window.location.host}/ws/chat-pdf/${username}/${roomName}/`);
+}
 
 
-// chatSocket.onmessage = function (e) {
-    // const pElement = document.createElement("p");
-    // const data = JSON.parse(e.data)
-    
-    // console.log("Data:", data);
-    // if (data.type == 'response') 
-    // {
-    //     pElement.textContent = data.message;
-    //     console.log(data.message);
-    // }
-    // else{
-    //     pElement.classList.add("error");
-    //     pElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
-    // }
-    // const incomingChatDiv = chatResponseElement;
-    // incomingChatDiv.querySelector(".typing-animation").remove();
-    // incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
-    // localStorage.setItem("all-chats", chatContainer.innerHTML);
-    // chatContainer.scrollTo(0, chatContainer.scrollHeight);
-    // chatInput.focus();
-// }
+btnStart.addEventListener('click', () => {
+    if(!numDocuments.value)
+    {
+        alert("Preencha o número de documentos que devem ser retornados!");
+        return;
+    }
+        
+    chatSocket = new WebSocket(`ws://${window.location.host}/ws/analisar-candidaturas/${vacancy_vid}/${numDocuments.value}/`);
+        
+    chatSocket.onopen = function(e) {
+        console.log('Conexão estabelecida.');
+        // window.alert('Conexão estabelecida.');
 
-// chatSocket.onclose = function() {
-//     console.log('Conexão fechada.');
-//     // window.alert("Conexão fechada");
-// };
+        let child = `
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            Processando...
+        `;
+        btnStart.innerHTML=``;
+        btnStart.setAttribute('disabled', true);
+        btnStart.insertAdjacentHTML('beforeend', child);
+        chatSocket.send(JSON.stringify({"message": "data"}))
+    };
+        
 
-/***************************************************** Events  ****************************************************/
-// sendButton.addEventListener("click", handleOutgoingChat);
-
-// themeButton.addEventListener("click", () => {
-//     document.body.classList.toggle("light-mode");
-//     localStorage.setItem("themeColor", themeButton.innerText);
-// });
-
-
-// chatInput.addEventListener("input", () => {
-//     chatInput.style.height = `${initialInputHeight}px`;
-//     chatInput.style.height = `${chatInput.scrollHeight}px`;
-// });
-
-// chatInput.addEventListener("keydown", (e) => {
-//     if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-//         e.preventDefault();
-//         handleOutgoingChat();
-//     }
-// });
-
-
-// deleteButton.addEventListener("click", () => {
-//     if (confirm("Tem a certeza de que deseja eliminar toda a conversa?")) 
-//     {
-//         localStorage.removeItem("all-chats");
-//         loadDataFromLocalstorage();
-//         clearInput();
-//     }
-// });
-
-// loadDataFromLocalstorage();
-
-// window.addEventListener("load", ()=>{
-    // chatContainer.querySelector(".default-text")?.remove();
-// })
+    chatSocket.onmessage = function (e) {
+        const data = JSON.parse(e.data)
+        console.log("Data Received:", data);
+        try {
+            if(data.type==='response')
+            {
+                removeForm();
+                alterTable(data.data);
+            }
+        } 
+        catch (error) 
+        {
+            console.error(error);
+            alert("Error ao ler os dados dos Cvs");
+        }
+    }
+        
+    chatSocket.onclose = function() {
+        console.log('Conexão fechada.');
+        // window.alert("Conexão fechada");
+    };
+        
+});
