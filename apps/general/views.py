179c78e8cc871aa80518e604074277922a651dd4
eriveltoSilva <eriveltoclenio@gmail.com>
2024-05-26 14:34:31 +0100

    feat: add admin menu option

--- apps/general/views.py
+++ apps/general/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
-class LandingPageView(generic.TemplateView):
+class LandingPageView(generic.View):
     template_name = 'landing-page.html'
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('accounts:controller'))
+        return render(self.request, self.template_name, {})
+         
+landing_page_view = LandingPageView.as_view()
     
